e3e8f3dde783da84a18a0b7085eb0c2ccc294e94
Luis Calle Lazo <callelazodeynarluis@gmail.com>
2023-11-07 15:59:30 -0400

    vr_rat_dey_001

---
+++ VR_RATLAND2/Assets/Scripts/AnimateHandOnInput.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+    public Animator HandAnimator;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        HandAnimator.SetFloat("Trigger", triggerValue);
+
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        HandAnimator.SetFloat("Grip", gripValue);
+    }
+}
